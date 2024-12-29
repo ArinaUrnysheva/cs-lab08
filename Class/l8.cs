96cc0a4194c5ac36b28ea3535097e1d5ad27f1c3
ArinaUrnysheva <148492062+ArinaUrnysheva@users.noreply.github.com>
2024-12-29 14:46:52 +0300

    Add files via upload

---
+++ Class/l8.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace lab8
+{
+    // Объявление пользовательского атрибута MyAttribute, который наследует от класса Attribute.
+    public class MyAttribute : Attribute
+    {
+        public string Comment { get; set; } // Свойство для хранения комментария.
+
+        // Конструктор без параметров.
+        public MyAttribute() { }
+
+        // Конструктор с параметром, который инициализирует свойство Comment.
+        public MyAttribute(string Comment)
+        {
+            this.Comment = Comment;
+        }
+    }
+
+    // Объявление абстрактного класса Animal с атрибутом MyAttribute.
+    [MyAttribute("Базовый класс Animal для всех животных")]
+    public abstract class Animal
+    {
+        // Свойства класса Animal.
+        public string Country { get; set; }
+        public string HideFromOtherAnimals { get; set; }
+        public string Name { get; set; }
+        public string WhatAnimal { get; set; }
+        public string classification { get; set; }
+        public string favourite_food { get; set; }
+
+        // Конструктор класса Animal, инициализирующий свойства.
+        public Animal(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classification, eFavouriteFood favourite_food)
+        {
+            this.Country = Country;
+            this.HideFromOtherAnimals = HideFromOtherAnimals;
+            this.Name = Name;
+            this.WhatAnimal = WhatAnimal;
+            this.classification = classification.ToString();
+            this.favourite_food = favourite_food.ToString();
+        }
+
+        // Метод для распаковки свойств Animal в отдельные переменные.
+        public void Deconstruct(out string country, out string hideFromOtherAnimals, out string name, out string whatAnimal)
+        {
+            country = Country;
+            hideFromOtherAnimals = HideFromOtherAnimals;
+            name = Name;
+            whatAnimal = WhatAnimal;
+        }
+
+        // Виртуальные методы, которые могут быть переопределены в производных классах.
+        public virtual void GetClassificationAnimal() { }
+        public virtual void GetFavouriteFood() { }
+        public virtual void SayHello() { }
+
+        // Метод для сериализации объекта Animal
+        /*public void Serialize(string fileName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Animal));
+            using (FileStream stream = new FileStream(fileName, FileMode.Create))
+            {
+                serializer.Serialize(stream, this);
+            }
+        }*/
+
+        // Статический метод для десериализации объекта Animal
+        public static Animal Deserialize(string fileName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Animal));
+            using (FileStream stream = new FileStream(fileName, FileMode.Open))
+            {
+                return (Animal)serializer.Deserialize(stream);
+            }
+        }
+    }
+
+    // Класс Cow (Корова), производный от Animal.
+    [MyAttribute("Производный от Animal класс Cow")]
+    public class Cow : Animal
+    {
+        // Конструктор класса Cow, вызывающий базовый конструктор Animal.
+        public Cow(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classification, eFavouriteFood favourite_food)
+            : base(Country, HideFromOtherAnimals, Name, WhatAnimal, classification, favourite_food) { }
+
+        // Переопределение метода для вывода классификации животного.
+        public override void GetClassificationAnimal()
+        {
+            Console.WriteLine($"{this.Name} классифицируется как {classification}");
+        }
+
+        // Переопределение метода для вывода любимой еды животного.
+        public override void GetFavouriteFood()
+        {
+            Console.WriteLine($"У {this.Name} любимая еда - это {favourite_food}");
+        }
+
+        // Переопределение метода SayHello для издания звука коровы.
+        public override void SayHello()
+        {
+            Console.WriteLine("муу");
+        }
+    }
+
+    // Класс Lion (Лев), производный от Animal.
+    [MyAttribute("Производный от Animal класс Lion")]
+    public class Lion : Animal
+    {
+        // Конструктор класса Lion, вызывающий базовый конструктор Animal.
+        public Lion(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classification, eFavouriteFood favourite_food)
+            : base(Country, HideFromOtherAnimals, Name, WhatAnimal, classification, favourite_food) { }
+
+        // Переопределение метода для вывода классификации животного.
+        public override void GetClassificationAnimal()
+        {
+            Console.WriteLine($"{this.Name} классифицируется как {classification}");
+        }
+
+        // Переопределение метода для вывода любимой еды животного.
+        public override void GetFavouriteFood()
+        {
+            Console.WriteLine($"У {this.Name} любимая еда - это {favourite_food}");
+        }
+
+        // Переопределение метода SayHello для издания звука льва.
+        public override void SayHello()
+        {
+            Console.WriteLine("ррр");
+        }
+    }
+
+    // Класс Pig (Свинья), производный от Animal.
+    [MyAttribute("Производный от Animal класс Pig")]
+    public class Pig : Animal
+    {
+        // Конструктор класса Pig, вызывающий базовый конструктор Animal.
+        public Pig(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classification, eFavouriteFood favourite_food)
+            : base(Country, HideFromOtherAnimals, Name, WhatAnimal, classification, favourite_food) { }
+
+        // Переопределение метода для вывода классификации животного.
+        public override void GetClassificationAnimal()
+        {
+            Console.WriteLine($"{this.Name} классифицируется как {classification}");
+        }
+
+        // Переопределение метода для вывода любимой еды животного.
+        public override void GetFavouriteFood()
+        {
+            Console.WriteLine($"У {this.Name} любимая еда - это {favourite_food}");
+        }
+
+        // Переопределение метода SayHello для издания звука свиньи.
+        public override void SayHello()
+        {
+            Console.WriteLine("хрю");
+        }
+    }
+
+    // Перечисление eClassificationAnimal для классификаций животных.
+    [MyAttribute("Перечисление классификаций животных")]
+    public enum eClassificationAnimal
+    {
+        Herbivores, // Травоядные
+        Carnivores, // Плотоядные
+        Omnivores   // Всеядные
+    }
+
+    // Перечисление eFavouriteFood для любимой еды животных.
+    [MyAttribute("Перечисление любимой еды животных")]
+    public enum eFavouriteFood
+    {
+        Meat,   // Мясо
+        Plants, // Растения
+        Everything // Всё
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,72 @@
+﻿using lab8; 
+using System; 
+using System.IO; 
+using System.Xml.Serialization; 
+
+namespace lAB8_serialization // Определяем пространство имен для этой программы
+{
+    class Program // Определяем главный класс программы
+    {
+        static void Main() // Главный метод, который будет исполнен при запуске программы
+        {
+            // ЗАДАНИЕ 1: Сериализация объекта Animal
+            Animal cow = new Cow("Russia", "No", "Bessie", "Cow", eClassificationAnimal.Herbivores, eFavouriteFood.Plants);
+            // Создаем экземпляр коровы с параметрами (страна, прятаться от других животных, имя, тип животного, категория и любимая еда)
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Animal)); // Реализация сериализации для класса Animal
+            using (TextWriter writer = new StreamWriter("animal.xml")) // Создаем потоку записи в файл "animal.xml"
+            {
+                serializer.Serialize(writer, cow); // Сериализуем объект cow и записываем его в файл
+            }
+
+            // Десериализация объекта Animal
+            XmlSerializer deserializer = new XmlSerializer(typeof(Animal)); // Создаем десериализатор для класса Animal
+            using (TextReader reader = new StreamReader("animal.xml")) // Открываем файл "animal.xml" для чтения
+            {
+                Animal deserializedAnimal = (Animal)deserializer.Deserialize(reader); // Десериализуем объект из файла
+                Console.WriteLine($"Deserialized Animal:"); // Выводим заголовок
+                // Выводим свойства десериализованного объекта на консоль
+                Console.WriteLine($"Country: {deserializedAnimal.Country}");
+                Console.WriteLine($"Hide From Other Animals: {deserializedAnimal.HideFromOtherAnimals}");
+                Console.WriteLine($"Name: {deserializedAnimal.Name}");
+                Console.WriteLine($"What Animal: {deserializedAnimal.WhatAnimal}");
+                Console.WriteLine($"Classification: {deserializedAnimal.classification}");
+                Console.WriteLine($"Favourite Food: {deserializedAnimal.favourite_food}");
+
+                // ЗАДАНИЕ 2: Поиск и вывод содержимого файла
+                string filePath = @"C:\Пользователи\Наталия\source\repos\l8\proj\bin\Debug"; // Путь к директории, где будет производиться поиск
+                string searchfile = "animal.xml"; // Имя файла для поиска
+                string[] files = Directory.GetFiles(filePath, searchfile, SearchOption.AllDirectories); // Поиск файла в указанной директории
+
+                if (files.Length > 0) // Если файл найден
+                {
+                    using (FileStream fileStream = File.OpenRead(files[0])) // Открываем найденный файл для чтения
+                    {
+                        byte[] buffer = new byte[fileStream.Length]; // Создаем буфер для хранения содержимого файла
+                        fileStream.Read(buffer, 0, buffer.Length); // Читаем содержимое файла в буфер
+                        Console.WriteLine(System.Text.Encoding.Default.GetString(buffer)); // Преобразуем байты в строку и выводим на консоль
+                    }
+                }
+                else // Если файл не найден
+                {
+                    Console.WriteLine("Файл не найден"); // Выводим сообщение об ошибке
+                }
+
+                // Сжатие файла animal.xml
+                string fileName = @"C:\Пользователи\Наталия\source\repos\l8\proj\bin\Debug\animal.xml"; // Полный путь к файлу
+                string compressedFilePath = $"{fileName}.gz"; // Путь для сохранения сжатого файла
+
+                using (FileStream sourceFile = File.OpenRead(fileName)) // Открываем файл animal.xml для чтения
+                {
+                    using (FileStream targetFile = File.Create(compressedFilePath)) // Создаем новый файл для сохранения сжатого содержимого
+                    {
+                        using (var compressor = new System.IO.Compression.GZipStream(targetFile, System.IO.Compression.CompressionMode.Compress)) // Создаем объект для сжатия
+                        {
+                            sourceFile.CopyTo(compressor); // Копируем содержимое исходного файла в сжатый файл
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
